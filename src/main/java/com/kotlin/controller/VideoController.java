9ec23132b3922a6ce231dc419daccdf0e0c91456
allen <jayx1213@126.com>
2019-05-10 19:25:19 +0800

    添加了上传功能的方法

--- src/main/java/com/kotlin/controller/VideoController.java
+++ src/main/java/com/kotlin/controller/VideoController.java
@@ -10,7 +10,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.util.Iterator;
 import java.util.List;
 
 @Controller
@@ -18,6 +25,8 @@ public class VideoController {
 
     @Autowired
     IVideoService videoService;
+
+    private static final String VIDEO_BASE_PATH = "";
 
     /**
      * 用户主要查询方法
@@ -82,6 +91,68 @@ public class VideoController {
         if (videoService.updateByPrimaryKeySelective(video) != 0) {
             return Msg.success();
         }
+        return Msg.fail();
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "video/upload")
+    public Msg uploadFile(HttpServletRequest request, HttpServletResponse response, Video video) {
+
+        try {
+
+            String path = "";
+            boolean isSuccess = false;
+            //将当前上下文初始化给  CommonsMutipartResolver （多部分解析器）
+            CommonsMultipartResolver multipartResolver=new CommonsMultipartResolver(
+                    request.getSession().getServletContext());
+
+            //检查form中是否有enctype="multipart/form-data"
+            if(multipartResolver.isMultipart(request)) {
+                //将request变成多部分request
+                MultipartHttpServletRequest multiRequest=(MultipartHttpServletRequest)request;
+                //获取multiRequest 中所有的文件名
+                Iterator iter=multiRequest.getFileNames();
+
+                while(iter.hasNext()) {
+                    //一次遍历所有文件
+                    MultipartFile file=multiRequest.getFile(iter.next().toString());
+
+                    File userFile = new File("D:\\Code\\IDE\\kotlinMvp\\src\\main\\webapp\\user\\" + video.getUserId() + "\\video\\");
+                    if (!userFile.exists()){
+                        userFile.mkdirs();
+                    }
+
+                    if(file!=null) {
+                        path ="D:\\Code\\IDE\\kotlinMvp\\src\\main\\webapp\\user\\" + video.getUserId() + "\\video\\" + file.getOriginalFilename();
+                        //上传
+                        file.transferTo(new File(path));
+                    }
+                    System.out.println(path);
+
+                }
+
+                if (videoService.insert(video) != 0 ){
+                    isSuccess = true;
+                    System.out.println("插入成功~~~~~~~~~~~~~~~~~~~~~~~");
+                } else {
+                    isSuccess = false;
+                    return Msg.fail();
+                }
+
+                if (isSuccess)
+                    return Msg.success();
+                else
+                    return Msg.fail();
+
+            } else {
+                System.out.println("[--------------------------------]没有enctype=multipart/form-data");
+                return Msg.fail();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return Msg.fail();
     }
 
